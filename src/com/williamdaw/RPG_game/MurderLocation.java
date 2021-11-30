ff198c3bc75d180253848c260868aad8339f6286
DoryBoy <williamodaw@outlook.com>
2021-11-30 10:37:31 +0000

    Revert "efrserdf"
    This reverts commit ce8f489b8ad544d3ef7fa3b4b86890f4224bb676.

--- src/com/williamdaw/RPG_game/MurderLocation.java
+++ src/com/williamdaw/RPG_game/MurderLocation.java
@@ -1,12 +1,7 @@
 package com.williamdaw.RPG_game;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 public class MurderLocation extends Room {
-    Room value;
-    public MurderLocation(House house) {
-        ArrayList<Room> rooms = house.get_rooms();
-        this.value = rooms.get(new Random().nextInt(rooms.size()));
+    public MurderLocation() {
+
     }
 }
