b4cb59c234b297d033d7592c82a841a50f136956
Daniel Judd <89047943+DanielJudd1@users.noreply.github.com>
2021-12-08 10:07:31 +0000

    Judd

--- src/com/williamdaw/RPG_game/PotentialMurderLocation.java
+++
@@ -1,9 +0,0 @@
-package com.williamdaw.RPG_game;
-
-public class PotentialMurderLocation {
-    String value;
-    public PotentialMurderLocation(String value) {
-        this.value = value;
-    }
-
-}
