54ee3944a4d753f2927edb307998bc361c22b784
DoryBoy <williamodaw@outlook.com>
2021-12-12 19:40:11 +0000

    adjasent_rooms addition

--- src/com/williamdaw/RPG_game/Room.java
+++ src/com/williamdaw/RPG_game/Room.java
@@ -2,12 +2,15 @@ package com.williamdaw.RPG_game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Room {
     String name;
     int floor;
     ArrayList<Object> items = new ArrayList<>();
     ArrayList<PotentialMurderLocation> potential_murder_locations = new ArrayList<PotentialMurderLocation>();
+
 
     public Room(String room_name) {
         this.name = room_name;
@@ -29,5 +32,22 @@ public class Room {
 
     public String get_name() {
         return this.name;
+    }
+
+    public void adjasent_rooms () {
+        Object[][] Guest = {{"Hallway1",1}};
+        Object[][] Master = {{"Hallway1",1},{"Upstairs Bathroom ",1}};
+        Object[][] child = {{"Hallway1",1}};
+        Object[][] Upstairs_Bathroom = {{"Hallway1",1}};
+        Object[][] Hallway1 = {{"Hallway0",0},{"Child",1},{"Master",1},{"Guest",1}};
+        Object[][] Hallway0 = {{"Hallway",1},{"Kitchen",0},{"Porch",0},{"Living_Room",0}};
+        Object[][] Porch = {{"Hallway0",0},{"Front_Garden",0}};
+        Object[][] Front_Garden = {{"Porch",0}};
+        Object[][] Living_Room = {{"Hallway0",0}};
+        Object[][] Kitchen = {{"Hallway0",0},{"Downstairs Bathroom",0},{"Back Garden",0}};
+        Object[][] Downstairs_Bathroom = {{"Hallway0",0}};
+        Object[][] Back_Garden = {{"Hallway0",0}};
+
+
     }
 }
