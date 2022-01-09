4f80a92801451a1ede183b34cd8e93ed09fb4070
martinnaj <martin@najemi.cz>
2022-01-09 22:32:32 +0000

    Update PotentialMurderLocation.java

--- src/com/williamdaw/RPG_game/PotentialMurderLocation.java
+++ src/com/williamdaw/RPG_game/PotentialMurderLocation.java
@@ -4,8 +4,4 @@ public class PotentialMurderLocation extends Item {
     public PotentialMurderLocation(String value) {
         super(value);
     }
-    public String get_potential_murder_location_name() {
-        return this.value;
-    }
-
 }
