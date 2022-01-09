9a85cbd78b01ce04c0c1d5754336f23fe874a505
DoryBoy <williamodaw@outlook.com>
2022-01-09 21:35:09 +0000

    Mwilll

--- src/com/williamdaw/RPG_game/PotentialMurderLocation.java
+++ src/com/williamdaw/RPG_game/PotentialMurderLocation.java
@@ -1,13 +1,11 @@
 package com.williamdaw.RPG_game;
 
 public class PotentialMurderLocation extends Item {
-    String Potential_name;
     public PotentialMurderLocation(String value) {
         super(value);
-        Potential_name = this.value;
     }
     public String get_potential_murder_location_name() {
-        return this.Potential_name;
+        return this.value;
     }
 
 }
