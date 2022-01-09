6aa97dd3c0ed69585f8ef0bc8c10e4f8116187fd
martinnaj <martin@najemi.cz>
2022-01-09 22:43:50 +0000

    Update House.java

--- src/com/williamdaw/RPG_game/House.java
+++ src/com/williamdaw/RPG_game/House.java
@@ -8,6 +8,7 @@ public class House {
     private MurderLocation murder_location = null;
     private final ArrayList<Room> rooms = new ArrayList<>();
     public Room start_location;
+
     public void set_murder_location() {
         this.murder_location = new MurderLocation(this);
     }
@@ -15,20 +16,21 @@ public class House {
     public void add_room(Room room) {
         this.rooms.add(room);
     }
-    public void add_room(Room[] rooms) {
-        Collections.addAll(this.rooms, rooms);
-    }
+
     public boolean has_room(String room_name) {
         for (com.williamdaw.RPG_game.Room room : this.rooms) if (Objects.equals(room.get_name(), room_name)) return true;
         return false;
     }
+
     public Room get_room(String room_name) throws Exception {
         for (com.williamdaw.RPG_game.Room room : this.rooms) if (Objects.equals(room.get_name(), room_name)) return room;
         throw new Exception("Room not found");
     }
+
     public ArrayList<Room> get_rooms() {
         return this.rooms;
     }
+
     public MurderLocation get_murder_location() throws Exception {
         if (this.murder_location == null) throw new Exception("Murder location has not been set.");
         return this.murder_location;
