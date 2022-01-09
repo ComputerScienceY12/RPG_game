5a15dad8ff9c633394fef8a5b5ba1c2728ccffcc
martinnaj <martin@najemi.cz>
2022-01-09 22:32:27 +0000

    Update Item.java

--- src/com/williamdaw/RPG_game/Item.java
+++ src/com/williamdaw/RPG_game/Item.java
@@ -1,13 +1,11 @@
 package com.williamdaw.RPG_game;
 
 public class Item {
-    String properties;
-
-    String value;
+    private String value;
     public Item(String value) {
         this.value = value;
     }
-
-
-
+    public String get_value() {
+        return this.value;
+    }
 }
